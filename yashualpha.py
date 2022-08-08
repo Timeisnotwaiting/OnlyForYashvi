213f48190a6d2861442eadb9c8022368d190444c
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-08 15:55:32 +0530

    Create yashualpha.py

---
+++ yashualpha.py
@@ -0,0 +1,5 @@
+from pyrogram import Client, filters
+from pyrogram.filters import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from config import *
+
+YashuAlpha = Client(":Yashvi:", API_ID, API_HASH, BOT_TOKEN)
