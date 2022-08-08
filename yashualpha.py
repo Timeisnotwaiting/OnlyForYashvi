dd7f84da398e9e3da4dcb901978661d95e814647
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-08 18:31:01 +0530

    Update yashualpha.py

--- yashualpha.py
+++ yashualpha.py
@@ -13,10 +13,14 @@ YASHU = [5296178757, 5285667393, 5432903250]
 
 ALPHA = [1985209910, 5429087029]
 
+init = None
+
 YashuAlpha = Client(":Yashvi:", API_ID, API_HASH, BOT_TOKEN)
 
 @YashuAlpha.on_message(filters.command("kiss", "") & filters.user(YASHUALPHA))
 async def kiss(_, m):
+    global init
+    init = m.from_user.id
     if not m.reply_to_message and not m.reply_to_message.from_user.id in YASHUALPHA:
         return 
     if m.from_user.id == YASHUALPHA[3] or m.from_user.id == YASHUALPHA[4]:
@@ -27,4 +31,10 @@ async def kiss(_, m):
 @YashuAlpha.on_callback_query(filters.regex("k_accept"))
 async def cbquery(_, c: CallbackQuery):
     if c.data == "k_accept":
-        
+        if c.from_user.id == init:
+            return await c.answer()
+        if c.from_user.id not in YASHUALPHA:
+            return await c.answer("This is only for YASHU-ALPHA !", show_alert=True)
+        alpha_m = (await _.get_users(init)).username
+        yashu_m = (await _.get_users(c.from_user.id)).username
+        await c.message.reply_photo(choice(KISS), 
