e32d27b7c43720621093e68f38f9e713d465c645
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-08 16:15:39 +0530

    Update yashualpha.py

--- yashualpha.py
+++ yashualpha.py
@@ -2,4 +2,8 @@ from pyrogram import Client, filters
 from pyrogram.filters import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from config import *
 
+YASHUALPHA = [5296178757, 5285667393, 5432903250, 1985209910, 5429087029]
+
 YashuAlpha = Client(":Yashvi:", API_ID, API_HASH, BOT_TOKEN)
+
+@YashuAlpha.on_message(filters.command("kiss", "") & filters.user(YASHUALPHA))
