33d9b15896673f05dc5411cac7cae1746308bee5
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-08 16:19:19 +0530

    Update yashualpha.py

--- yashualpha.py
+++ yashualpha.py
@@ -7,3 +7,6 @@ YASHUALPHA = [5296178757, 5285667393, 5432903250, 1985209910, 5429087029]
 YashuAlpha = Client(":Yashvi:", API_ID, API_HASH, BOT_TOKEN)
 
 @YashuAlpha.on_message(filters.command("kiss", "") & filters.user(YASHUALPHA))
+async def kiss(_, m):
+    if not m.reply_to_message:
+        return 
